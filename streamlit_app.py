6d81fd2a4aef8002099b96beeb6194f04add59db
PhiSpel <62760840+PhiSpel@users.noreply.github.com>
2022-05-07 12:00:55 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,23 +5,37 @@ import streamlit as st
 #    page_view_counter+=1
 #    return page_view_counter
     
-@st.cache()
+@st.cache(allow_output_mutation=True)
 def global_page_views():
-    return global_page_views
+    gpv = 0 # this will only be run once because after the first execution the cache will be referenced
+    return gpv
     
-@st.chache()
-def session_execution_counter(session_executions):
-    session_executions+=1
-    return session_executions
+@st.chache(allow_output_mutation=True)
+def session_execution_counter():
+    sec = 0 # this will only be run once because after the first execution the cache will be referenced
+    return sec
+
+@st.chache(allow_output_mutation=True)
+def session_execution_counter_history():
+    sec_history = [] # this will only be run once because after the first execution the cache will be referenced
+    return sec_history
+
+st.title('Trying out page view counts with caching.')
     
-if not 'some_variable' in st.session_state:
-    gpv = global_page_views()
-    gpv += 1
+if not 'session_executions' in st.session_state:
+    # this will only be run once per session
+    gpv = global_page_views() # initialize the global page views variable (activates the cache)
+    gpv += 1 # this should raise the global_page_views() output by one globally
     #page_views = page_view_counter(page_views)
-    st.session_state['some_variable'] = 'something'
-    
-session_executions = 0
-session_execution_counter(session_executions)
+    sec = session_execution_counter() # get the sec from previous session
+    sec_history = session_execution_counter_history() # get sec history from global cache
+    sec_history.append(sec) # append sec from previous session to global counter
+    sec = 1 # this will globally set the sec to 1
+    st.session_state.session_executions = 'something'
+else:
+    sec = session_execution_counter()
+    sec += 1
+session_execution_counter(session_executions) # returns one more than previously and caches it
     
 st.write('current page views: ' + str(page_views))
 st.write('current session executions: ' + str(session_executions))
