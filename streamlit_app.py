12233bf23b6ba351c3ecd9ff1bfc43a04b3ba3fc
PhiSpel <62760840+PhiSpel@users.noreply.github.com>
2022-05-09 13:26:00 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,8 +29,9 @@ if not 'session_executions' in st.session_state:
     st.session_state.session_executions = 'something'
     st.session_state.gpv = gpv
 else:
-    sec = session_execution_counter() # getting gpv from global cache
+    sec = session_execution_counter() # getting sec from global cache
     sec += 1 # returns one more than it previously was and caches it (as it is related to the cached function with a hash)
+    gpv = global_page_views() # getting gpv from global cache
     
     
 st.write('current page views: ' + str(gpv))
