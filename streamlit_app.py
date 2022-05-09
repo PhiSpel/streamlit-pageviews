dcca210a71dbb693eed7cc902e4e0d1936a2b04f
PhiSpel <62760840+PhiSpel@users.noreply.github.com>
2022-05-09 14:32:03 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from streamlit import session_state as state
-import streamlit_server_state
 from streamlit_server_state import server_state, server_state_lock
 
 st.title("Pageview Counter Example")
@@ -9,10 +8,10 @@ with server_state_lock["gpv"]:  # Lock the "gpv" state for thread-safety
     if "gpv" not in server_state:
         server_state.gpv = 0
         
-# initialize page
+# initialize session execution count
 if 'sec' not in state:
     state.sec = 0
-    server_state.gpv += 1
+    server_state.gpv += 1 # this should only be done on start of the page, so, the session state setup is a good place
 else:
     state.sec += 1
 
