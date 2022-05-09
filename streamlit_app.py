51e4fbacd2e5ff4fbeaa74cde917ffb4cdcc2288
PhiSpel <62760840+PhiSpel@users.noreply.github.com>
2022-05-09 13:36:53 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,18 @@
 import streamlit as st
 
-@st.cache()
-def global_page_views():
-    gpv = 0 # this will only be run once because after the first execution the cache will be referenced
+@st.cache(allow_output_mutation=True)
+def global_page_views(gpv = 0):
+    gpv += 1 # this will only be run once because after the first execution the cache will be referenced
     return gpv
     
-@st.cache()
-def session_execution_counter():
-    sec = 0 # this will only be run once because after the first execution the cache will be referenced
+@st.cache(allow_output_mutation=True)
+def session_execution_counter(sec = 0):
+    sec += 1 # this will only be run once because after the first execution the cache will be referenced
     return sec
 
-@st.cache()
-def session_execution_counter_history():
-    sec_history = [] # this will only be run once because after the first execution the cache will be referenced
+@st.cache(allow_output_mutation=True)
+def session_execution_counter_history(sec_history = []):
+    sec_history.append(sec_history) # this will only be run once because after the first execution the cache will be referenced
     return sec_history
 
 st.title('Trying out page view counts with caching.')
@@ -20,19 +20,14 @@ st.title('Trying out page view counts with caching.')
 if not 'session_executions' in st.session_state:
     # this will only be run once per session
     gpv = global_page_views() # initialize the global page views variable (activates the cache)
-    gpv += 1 # this should raise the global_page_views() output by one globally
+    gpv = global_page_views(gpv) # updating gpv by 1
     #page_views = page_view_counter(page_views)
     sec = session_execution_counter() # get the sec from previous session
     sec_history = session_execution_counter_history() # get sec history from global cache
-    sec_history.append(sec) # append sec from previous session to global counter
-    sec = 1 # this will globally set the sec to 1
-    st.session_state.session_executions = 'something'
-    st.session_state.gpv = gpv
 else:
-    sec = session_execution_counter() # getting sec from global cache
-    sec += 1 # returns one more than it previously was and caches it (as it is related to the cached function with a hash)
-    gpv = global_page_views() # getting gpv from global cache
-    sec_history = session_execution_counter_history() # getting sec_history from global cache
+    sec = session_execution_counter(sec) # getting sec from global cache
+    gpv = global_page_views()
+    sec_history = session_execution_counter_history(sec_history)
     
     
 st.write('current page views: ' + str(gpv))
